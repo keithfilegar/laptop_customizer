6ea466fd7bf8be9a0e342e74850ea13369ee0c0a
keithfilegar <keith.filegar@gmail.com>
2020-12-16 15:16:44 -0800

    move new code to original .App file, move FEATURES into Features.js

--- src/App1.js
+++ src/App1.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-
-// Normalizes string as a slug - a string that is safe to use
-// in both URLs and html attributes
-import slugify from 'slugify';
-
 import './App.css';
 import CartList from './CartList/CartList';
 import LaptopOptionForm from './LaptopOptionForm/LaptopOptionForm'
+import FEATURES from './Features'
 
 // This object will allow us to
 // easily convert numbers into US dollar values
@@ -14,49 +10,6 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
   style: 'currency',
   currency: 'USD'
 });
-
-const FEATURES = {
-    Processor: [
-      {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
-        cost: 700
-      },
-      {
-        name: 'Professor X AMD Fire Breather with sidewinder technology',
-        cost: 1200
-      }
-    ],
-    "Operating System": [
-      {
-        name: 'Ubuntu Linux 16.04',
-        cost: 200
-      },
-      {
-        name: 'Bodhi Linux',
-        cost: 300
-      }
-    ],
-    "Video Card": [
-      {
-        name: 'Toyota Corolla 1.5v',
-        cost: 1150.98
-      },
-      {
-        name: 'Mind mild breeze 2000',
-        cost: 1345
-      }
-    ],
-    Display: [
-      {
-        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
-        cost: 1500
-      },
-      {
-        name: '15.3" HGTV (3840 x 2160) Home makeover edition',
-        cost: 1400
-      },
-    ]
-};
 
 class App1 extends Component {
     state = {
